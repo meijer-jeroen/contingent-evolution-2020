12a1e0c381259394eccf333c75dc71a2564ca3ad
Jeroen Meijer <meijeroen@gmail.com>
2020-05-24 00:30:59 +0200

    fixed supplement 5 script

--- src/supplements/suppl_fig_5_community_pca.R
+++ src/supplements/suppl_fig_5_community_pca.R
@@ -1,75 +1,130 @@
+
+# make pca ----------------------------------------------------------------
+library(colorspace)
 library('tidyverse')
+library('ggrepel')
 library('cowplot')
-load('../data/temp/mgx_commsbio2020.rdata') # first run read_save_mgx.R to collect data
+load('data/temp/fig4.rdata') # first run fig4_heatmap/fig4_read_data.R
 
-community_cols <- c('gold4', "#0072B2")
-colnames(mgx)[5] <- 'cur_commtype'
-label_cols <- c('time_point',
-                'sim',
-                'seed',
-                'cur_commtype',
-                'final_commtype',
-                'population',
-                'ene_type',
-                'ene_react')
+# remove two outlier populations. See SI5_PCA_all_communties.R for analysis including outliers.
+#for_pca <- filter(mgx, seed != 906)
+#for_pca <- filter(for_pca, seed != 706)
+for_pca <- mgx
 
-labels <- select(mgx, label_cols)
-for_pca <- select(mgx, -label_cols)
+label_cols <-
+  c('time_point',
+    'sim',
+    'seed',
+    'population',
+    'cur_commtype', # community type at current time point
+    'final_commtype', 
+    'ene_type', # topology of metabolic network, estimated by relative frequencies in population
+    'ene_react') # most common energy reaction in community
 
-var0 <- unlist(lapply(for_pca, function(x)
-  0 == var(if (is.factor(x))
-    as.integer(x)
-    else
-      x)))
+labels <- select(for_pca, label_cols)
+for_pca <- select(for_pca, -label_cols)
 
+var0 <-
+  unlist(lapply(for_pca, function(x)
+    0 == var(if (is.factor(x))
+      as.integer(x)
+      else
+        x)))
 for_pca <- for_pca[, !var0]
 
 pca <- prcomp(for_pca, center = TRUE, scale = TRUE)
 PoV <- pca$sdev ^ 2 / sum(pca$sdev ^ 2)
+pca <- cbind(labels, pca$x[, c(1:2)]) # only save PC1 and PC2
 
-# re-attach labels
-pca <- cbind(labels, pca$x[, c(1:3)])
+community_cols <- c('gold4', "#0072B2")
+topology_cols <- c("#56B4E9","#F0E442", "#009E73","#999999")
 
-# make the plot
-all <- ggplot(pca) +
+
+# Plot Figure 5.a-d         -----------------------------------------------
+
+selected <- c(104, 203, 701)
+
+fig <- ggplot(pca) +
+  # data coloured by sim
   geom_point(
     data =  pca %>%
       group_by(seed) %>%
-      mutate(time_max = max(time_point)) %>% 
+      mutate(time_max = max(time_point)) %>%
       ungroup()  %>%
       filter(time_point == time_max),
-    aes(x = PC1, y = PC2, fill = final_commtype),
-    col = 'white',
-    size = 2.5,
-    shape = 21,
-    alpha = 0.8
+    aes(x = PC1, y = PC2, shape = final_commtype, fill = ene_type, col = ene_type),
+    size = 4,
+    alpha = 0.55
   )  +
-  scale_fill_manual(name = NULL,
-                    values = community_cols)  +
+  scale_shape_manual(name = 'Community type',
+                     values = c(24, 21, 22)) +
+
+  scale_colour_manual(name = 'Network topology',
+                    breaks = c('resource', 'building block', 'hybrid'),
+                    values = darken(topology_cols, amount = 0.3, space = "HCL"), 
+                    limits = c('resource', 'building block', 'hybrid'), # change order of legend items
+                    labels = c('Resource', 'Building block', 'Hybrid') # capitals
+  ) +
+  scale_fill_manual(name = 'Network topology',
+                    breaks = c('resource', 'building block', 'hybrid'),
+                    values = (topology_cols),
+                    limits = c('resource', 'building block', 'hybrid'), # change order of legend items
+                    labels = c('Resource', 'Building block', 'Hybrid') # capitals
+  ) + 
   
   # add start marker
+  geom_text_repel(data = filter(pca,
+                                time_point == 0 & seed == 101),
+                  aes(x = PC1, y = PC2),
+                  label = 'start',
+                  nudge_x = -.7, 
+                  nudge_y = .3)  +
   geom_point(
     data = filter(pca,
                   time_point == 0) %>% 
-      filter(seed == unique(mgx$seed[1])),
+      filter(seed == unique(pca$seed[1])),
     aes(x = PC1, y = PC2),
-    size = 2,
-    shape = 21,
-    colour = 'black',
-    fill = 'white'
+    size = 4,
+    shape = 18,
+    colour = 'black'
   ) +
-  theme_bw() + 
-  ggtitle('Supplementary Figure 5. \nPCA of metabolic networks, including \n2 outlier communties not shown in Fig. 5.a \nof the main text')  + 
-  xlab(paste0('PCA (', 100 * round(PoV[1], digits = 4), '%)')) +
-  ylab(paste0('PCA (', 100 * round(PoV[2], digits = 4), '%)')) +
-  coord_fixed()
+  
+  #mark outliers
+  geom_text_repel(data = filter(pca,
+                                time_point == 1000000 & seed %in% c(906,706)
+                                ),
+                  aes(x = PC1, y = PC2, label = population), 
+                  nudge_x = -0.5, 
+                  nudge_y = -0.5
+                 ) +
+  
+  theme_minimal_grid() + 
+  ggtitle('') + 
+  xlab(paste0('PC1 (', 100 * round(PoV[1], digits = 4), '%)')) +
+  ylab(paste0('PC2 (', 100 * round(PoV[2], digits = 4), '%)')) +
+  coord_fixed() +
+  
+  # Add invisible line to keep axis identical to figure bcd
+  geom_path(data = pca %>%
+              filter(seed %in% selected) %>%
+              filter(time_point %in% seq(0, 1000000, by = 2500)) %>%
+              select(-sim),
+            aes(x = PC1,
+                y = PC2,
+                group = seed),
+            alpha = 0
+            ) + 
+  panel_border(color = 'black') 
 
 
-save_plot("../results/figures/supplementary_figure_5.pdf", 
-          all, 
-          #nrow = 2, 
-          #base_asp = 3.5, 
+save_plot("figures/supplement_5_pca.pdf", 
+          fig, 
+          nrow = 1, 
+          base_asp = 1, 
           base_height = NULL, 
           base_width = 8)
 
+
+
+  
 
