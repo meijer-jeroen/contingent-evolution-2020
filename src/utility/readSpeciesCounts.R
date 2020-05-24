1ebdd3230cd18f1ca054d8224b55700c060d89d9
Jeroen Meijer <meijeroen@gmail.com>
2020-05-24 17:11:30 +0200

    added packages to requirements.txt

--- src/utility/readSpeciesCounts.R
+++ src/utility/readSpeciesCounts.R
@@ -2,7 +2,6 @@
 readSpeciesCounts <- function(folder)  {
   library(data.table)
   cat('reading species_counts.csv', '\n')
-  #species_counts <- read.csv(file.path(folder, '/data/population_dat/species_counts.csv'))
   species_counts <- as.data.frame(fread(file.path(folder, '/data/population_dat/species_counts.csv')))  
   species_counts <- species_counts[, !apply (is.na(species_counts), 2, all)] # remove rows that are all NA
   species_counts[is.na(species_counts)] <- 0 
